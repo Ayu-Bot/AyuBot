7fbe6a3ec6c4a694303928529569855c2144c943
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-01 17:28:01 +0000

    setlang updated

--- commands/config/setlang.js
+++ commands/config/setlang.js
@@ -9,15 +9,15 @@ module.exports = {
 			option.setName('lang')
 				.setDescription('Linguagem')
 				.setRequired(true)
-				.addChoices({name: 'English', value: 'en-us'},  {name:'Português Brasileiro', value: 'pt-br'}, {name: 'Italiano', value: 'it-it'}, {name: "Türkçe", value: "tr-tr"}, {name:"UwU English", value: "en-uwu"}, {name:"Deutsch", value:"de-de"})
-										),
+				.addChoices({ name: 'English', value: 'en-us' }, { name: 'Português Brasileiro', value: 'pt-br' }, { name: 'Italiano', value: 'it-it' }, { name: "Türkçe", value: "tr-tr" }, { name: "UwU English", value: "en-uwu" }, { name: "Deutsch", value: "de-de" }, { name: "Français Canadien", value: "fr-ca" })
+		),
 
 	async execute(client, interaction) {
 		const user = await client.db.user.findOne({ _id: interaction.user.id });
 		if (!user) return;
-		const lang = client.lang(user.lang)
-    user.lang = interaction.options.getString('lang');
+		user.lang = interaction.options.getString('lang');
 		user.save();
+		const lang = client.lang(user.lang)
 		interaction.reply(lang.setlang.success.text)
 	},
 
